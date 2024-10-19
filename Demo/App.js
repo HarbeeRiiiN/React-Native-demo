11c122d254a08bb2b8e4915fe26acaf94df32d27
HarbeeRiiiN <harbeeriiin@outlook.com>
2024-10-19 03:30:02 -0700

    debug done

---
+++ Demo/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  console.log("step1...");
+
+  console.log("step2...");
+  // let x;
+  // x.toString();
+
+  return (
+    <View style={styles.container}>
+      <Text>Hello!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
